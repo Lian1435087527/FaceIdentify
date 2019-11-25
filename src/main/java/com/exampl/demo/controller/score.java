64a901d24e546b4ee08c60969190c10b0bdacc04
lt970915 <1819783515@qq.com>
2019-11-25 13:56:04 +0800

    13254655

---
+++ src/main/java/com/exampl/demo/controller/score.java
@@ -0,0 +1,54 @@
+package com.exampl.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.exampl.demo.baiduapi.GsonUtils;
+import com.exampl.demo.baiduapi.HttpUtil;
+import com.exampl.demo.faceidentify.BaseFunctionsBaidu;
+import com.exampl.demo.faceidentify.Normalimage;
+import com.exampl.demo.faceidentify_i.BaseFunctions_I;
+import com.exampl.demo.faceidentify_i.Normalimage_I;
+import com.exampl.demo.token.UserLoginToken;
+
+@Controller
+public class score {
+	
+	private String url_p="http://461af0d4-8da9-43ee-8285-e8d2619d98aa.southeastasia.azurecontainer.io/score";
+	@UserLoginToken
+	@RequestMapping("/score")
+	@ResponseBody
+	public ModelMap getul(@RequestParam("url") String url_g) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("url", url_g);
+		
+
+		String param = GsonUtils.toJson(map);
+		String accessToken=null;
+		String result = HttpUtil.post(url_p,accessToken, "application/json", param);
+		
+		ModelMap Return=new ModelMap();
+			
+		 Return.put("result", result);
+		
+		
+		
+		
+		return Return;
+		
+
+		}
+	
+	
+
+
+
+}
