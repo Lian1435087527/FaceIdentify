03724395b6bd069372d58b42426dd14fe986716c
lt970915 <1819783515@qq.com>
2019-12-13 11:23:20 +0800

    文件系统初版

--- src/main/java/com/exampl/demo/controller/score.java
+++ src/main/java/com/exampl/demo/controller/score.java
@@ -20,16 +20,26 @@ import com.exampl.demo.token.UserLoginToken;
 @Controller
 public class score {
 
-	private String url_p = "http://a5b2350c-5154-49e4-82bd-958b9ce1703c.southeastasia.azurecontainer.io/score";
-
+	private String url_p1 = "http://a5b2350c-5154-49e4-82bd-958b9ce1703c.southeastasia.azurecontainer.io/score";
+	private String url_p2 ="http://104.45.95.31:80/api/v1/service/facelivenessirsl/score";
 	@UserLoginToken
 	@RequestMapping("/score")
 	@ResponseBody
-	public ModelMap getul(@RequestParam("url") String url_g) throws Exception {
+	public ModelMap getul(@RequestParam("url") String url_g,@RequestParam("modelname") String modelname) throws Exception {
 
 		String param = "{\"url\":\"" + url_g + "\"}";
 		// System.out.println(param);
-		String authorization = null;
+          String url_p=null;
+          String authorization=null;
+		switch (modelname){
+			case "faceliveness":
+				 url_p = url_p1;
+				 authorization = null;
+				 break;
+			case "facelivenessirsl":
+				 url_p=url_p2;
+				authorization ="Bearer ps2vNRlAnRf2SKlUvd25p95AMXshNvrC";
+		         break;}
 		String result = HttpUtil.post(url_p, authorization, "application/json", param);
 		//处理返回字符串
 		String resultDic=result.substring(2,result.length()-2);
