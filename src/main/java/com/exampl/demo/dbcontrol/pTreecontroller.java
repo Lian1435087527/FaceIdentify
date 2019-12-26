1451926731f5c336ec8eddaa1fba583b8e907e0f
lt970915 <1819783515@qq.com>
2019-12-26 11:31:39 +0800

    文件系统终版五

--- src/main/java/com/exampl/demo/dbcontrol/pTreecontroller.java
+++ src/main/java/com/exampl/demo/dbcontrol/pTreecontroller.java
@@ -12,7 +12,7 @@ import com.exampl.demo.model.Tree;
 
 import java.util.List;
 import java.util.Map;
-
+import org.json.JSONArray;
 @Controller
 @RequestMapping("/posttree")
 public class pTreecontroller {
@@ -21,17 +21,26 @@ public class pTreecontroller {
 
     @PostMapping
 
-    public @ResponseBody ModelMap posttree(@RequestBody String ltree) {
+    public @ResponseBody ModelMap posttree(@RequestParam("tree") String ltree) {
 
         ModelMap Return=new ModelMap();
-        System.out.println(ltree);
-        /*if(TreeRepository.savet(tree)==0) {
-            Return.put("state", 0);
+        Tree tree2 = new Tree();
+        //System.out.println(ltree);
+        JSONArray tree1=new JSONArray(ltree);
+
+        for(int i=0;i<tree1.length();i++){
+            tree2.setTree_id(tree1.getJSONObject(i).getInt("t_id"));
+            tree2.setName(tree1.getJSONObject(i).getString("t_name"));
+            tree2.setPid(tree1.getJSONObject(i).getInt("t_pid"));
+            System.out.println(tree2);
+            if(TreeRepository.savet(tree2)==0) {
+                Return.put("state", 0);
+            }
+            else {
+                Return.put("state", 1);}
+
         }
-        else {
-            Return.put("state", 1);}
-        return Return;*/
-        Return.put("state", 1);
+
         return Return;
 
 
